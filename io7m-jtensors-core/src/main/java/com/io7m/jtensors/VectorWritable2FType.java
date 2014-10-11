07a7e49b6bdb15d7ccadf5e2f554af3e8ce40320
Mark Raynsford <code@io7m.com>
2014-10-11 18:10:52 +0000

    Reorganize into core/doc packages

---
+++ io7m-jtensors-core/src/main/java/com/io7m/jtensors/VectorWritable2FType.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright © 2014 <code@io7m.com> http://io7m.com
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.io7m.jtensors;
+
+/**
+ * <p>
+ * 'Write' interface to two-dimensional vectors with single precision
+ * elements.
+ * </p>
+ */
+
+public interface VectorWritable2FType
+{
+  /**
+   * Set the current vector to the contents of the input vector.
+   * 
+   * @param in_v
+   *          The input vector
+   */
+
+  void copyFrom2F(
+    VectorReadable2FType in_v);
+
+  /**
+   * Set the current <code>x</code> and <code>y</code> values of the current
+   * vector.
+   * 
+   * @param x
+   *          The new <code>x</code> value.
+   * @param y
+   *          The new <code>y</code> value.
+   */
+
+  void set2F(
+    float x,
+    float y);
+
+  /**
+   * Set the current <code>x</code> value of the current vector.
+   * 
+   * @param x
+   *          The new <code>x</code> value.
+   */
+
+  void setXF(
+    float x);
+
+  /**
+   * Set the current <code>y</code> value of the current vector.
+   * 
+   * @param y
+   *          The new <code>y</code> value.
+   */
+
+  void setYF(
+    float y);
+}
