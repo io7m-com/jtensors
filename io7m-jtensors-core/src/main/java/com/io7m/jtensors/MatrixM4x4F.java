5fc48c236d8016b2d518a2f258dc927ff094024f
Mark Raynsford <code@io7m.com>
2014-11-30 13:16:48 +0000

    Ignore boxing warnings

--- io7m-jtensors-core/src/main/java/com/io7m/jtensors/MatrixM4x4F.java
+++ io7m-jtensors-core/src/main/java/com/io7m/jtensors/MatrixM4x4F.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- *
+ * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -2616,7 +2616,7 @@ public final class MatrixM4x4F implements
     return this;
   }
 
-  @Override public String toString()
+  @SuppressWarnings("boxing") @Override public String toString()
   {
     final StringBuilder builder = new StringBuilder();
     for (int row = 0; row < MatrixM4x4F.VIEW_ROWS; ++row) {
